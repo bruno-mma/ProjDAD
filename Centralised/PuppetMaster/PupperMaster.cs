e4def6b36c50b8c9ab7ac6276f9e33371e6186e7
jozeus <jozeus.alves@gmail.com>
2019-11-01 19:05:10 +0000

    puppetMaster reads commands either from a file or  from the console

--- Centralised/PuppetMaster/PupperMaster.cs
+++ Centralised/PuppetMaster/PupperMaster.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace PuppetMaster
 {
-    class PupperMaster
+    class Program
     {
         static void Main()
         {
-            PuppetMaster puppetMaster = new PuppetMaster();
+            if (File.Exists("configFile.txt"))
+            {
+                try
+                {
+                    string[] configFileContents = File.ReadAllLines("configFile.txt");
+
+                    // TODO: make the puppet master execute the commands
+                    // for now it just prints them
+                    foreach (var item in configFileContents)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.ReadLine();
+                }
+            
+                catch (IOException e) {
+                    Console.WriteLine(e);
+                }
+            }
+
+            else
+            {
+                while (true)
+                {
+                    string command = Console.ReadLine();
+                    // TODO: execute command
+                }
+            }
         }
     }
 
