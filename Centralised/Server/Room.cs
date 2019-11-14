a39b9891ced75830f0aae6d8162693a042bed14c
Bruno Amorim <brunobolaxa124@gmail.com>
2019-11-14 11:00:55 +0000

    closed #35#

--- Centralised/Server/Room.cs
+++ Centralised/Server/Room.cs
@@ -6,15 +6,18 @@ using System.Threading.Tasks;
 
 namespace Server
 {
-	class Room
+	class Room : IComparable<Room>
 	{
 		public string _location;
 		public string _name;
 
 		public int _capacity;
 
-		//booked dates, date -> meeting topic, ex: "2019-11-14" -> "meme talk"
+		//booked dates (with closed meeting), date -> meeting topic, ex: "2019-11-14" -> "meme talk"
 		public Dictionary<string, string> _dates = new Dictionary<string, string>();
+
+		//used for determining the most desirable room for a meeting
+		public int NumAvailable { get; private set; }
 
 		public Room(string location, string room_name, int capacity)
 		{
@@ -22,6 +25,43 @@ namespace Server
 			_name = room_name;
 
 			_capacity = capacity;
+		}
+
+		public static int CompareRooms(Room r1, Room r2)
+		{
+			//if both have the same number of potential attending users
+			if (r1.NumAvailable == r2.NumAvailable)
+			{
+				//pick the one with the lowest capacity
+				if (r1._capacity > r2._capacity) return 1;
+				if (r1._capacity < r2._capacity) return -1;
+
+				return 0;
+			}
+
+			//pick the one with the highest number of potential attending users
+			if (r1.NumAvailable > r2.NumAvailable) return -1;
+			if (r1.NumAvailable < r2.NumAvailable) return 1;
+
+			return 0;
+		}
+
+		public int CompareTo(Room other)
+		{
+			return CompareRooms(this, other);
+		}
+
+		public void SetPotentialAttending(int interested)
+		{
+			if (interested > _capacity)
+			{
+				NumAvailable = _capacity;
+			}
+
+			else
+			{
+				NumAvailable = interested;
+			}
 		}
 	}
 }
--- Centralised/Server/Server.cs
+++ Centralised/Server/Server.cs
@@ -25,6 +25,7 @@ namespace Server
 			server.AddRoom("Lisboa", "Room-A", 20);
 			server.AddRoom("Lisboa", "Room-B", 10);
 			server.AddRoom("Porto", "Room-C", 15);
+			server.AddRoom("memeCity", "memeZone", 2);
 
 			RemotingServices.Marshal(server, "Server", typeof(IServer));
 
@@ -65,7 +66,9 @@ namespace Server
 			else
 			{
 				Console.WriteLine("Adding client: " + client_name + ", on port: " + port);
-				_clients.Add(client_name, client);
+				_clients[client_name] = client;
+
+				UpdateClientAllMeetings(client_name);
 			}
 
 			return true;
@@ -94,19 +97,147 @@ namespace Server
 			}
 		}
 
+		private void UpdateClientAllMeetings(string client_name)
+		{
+			foreach (Meeting meeting in _meetings.Values)
+			{
+				if (meeting.NumberOfInvitees == 0 || meeting.Invitees.Contains(client_name))
+				{
+					_clients[client_name].UpdateMeeting(meeting.MeetingTopic, meeting._meetingData);
+					Console.WriteLine("Updated client " + client_name + " with meeting " + meeting.MeetingTopic);
+				}
+			}
+		}
+
 
 		public bool CloseMeeting(string client_name, string meeting_topic)
 		{
-			throw new NotImplementedException();
+			//if meeting does not exist, cant close it
+			if (!_meetings.ContainsKey(meeting_topic))
+			{
+				return false;
+			}
+
+			Meeting meeting = _meetings[meeting_topic];
+
+			//only meeting owner can close a meeting
+			if (meeting.MeetingOwner != client_name)
+			{
+				return false;
+			}
+
+			List<Room> available_rooms = new List<Room>();
+			List<string> available_dates = new List<string>();
+
+			//for each of the possible locations, get available rooms
+			foreach (KeyValuePair<string, List<string>> kvp in meeting.MeetingRecords)
+			{
+				List<string> interested_users = kvp.Value;
+
+				// if this location does not have enough users interested, try the next one
+				if (interested_users.Count < meeting.MinAttendees)
+				{
+					continue;
+				}
+
+				string location = kvp.Key.Split(',')[0];
+				string date = kvp.Key.Split(',')[1];
+
+
+				//find out if location has room available
+				foreach (Room room in this._locations[location]._rooms.Values)
+				{
+					if (!room._dates.ContainsKey(date))
+					{
+						room.SetPotentialAttending(interested_users.Count);
+						available_rooms.Add(room);
+						available_dates.Add(date);
+					}
+				}
+			}
+
+			//if no room is available, cant close the meeting
+			if (available_rooms.Count == 0)
+			{
+				//TODO: maybe cancel meeting?
+				return false;
+			}
+
+			Room selected_room = available_rooms[0];
+			string selected_date = available_dates[0];
+
+			if (available_rooms.Count > 1)
+			{
+				//pick the room pick the room with the most potential attendees
+				for (int i = 1; i < available_rooms.Count; i++)
+				{
+					if (Room.CompareRooms(selected_room, available_rooms[i]) == 1)
+					{
+						selected_room = available_rooms[i];
+						selected_date = available_dates[i];
+					}
+				}
+			}
+
+			//schedule meeting
+			meeting.Closed = true;
+			meeting.SelectedDate = selected_date;
+			meeting.SelectedRoom = selected_room._location + ',' + selected_room._name;
+
+			string meeting_record = selected_room._location + ',' + selected_date;
+
+			//add selected users to users attending meeting
+			meeting.SelectedUsers = meeting.MeetingRecords[meeting_record].GetRange(0, selected_room.NumAvailable);
+
+			//book room at selected date
+			selected_room._dates.Add(selected_date, meeting.MeetingTopic);
+
+			//update clients
+			UpdateMeetingInvolvedClients(meeting);
+
+			return true;
 		}
 
-		public bool CreateMeeting(string owner_name, string meeting_topic, int min_attendees, int number_of_slots, int number_of_invitees, List<string> slots, List<string> invitees)
+		public bool CreateMeeting
+			(string owner_name, string meeting_topic, int min_attendees, int number_of_slots, int number_of_invitees, List<string> slots, List<string> invitees)
 		{
-            Meeting meeting = new Meeting(meeting_topic, owner_name, min_attendees, number_of_slots, number_of_invitees, slots, invitees);
+			//meeting topic has to be unique
+			if (_meetings.ContainsKey(meeting_topic))
+			{
+				return false;
+			}
 
-            _meetings.Add(meeting_topic, meeting);
+			bool valid_spot = false;
 
-			string print = "Client " + owner_name + " created a meeting with topic: " + meeting_topic + ", with " + min_attendees + " required atendees, with slots: ";
+			//for a slot to be valid, the location has to exist
+			for (int i = 0; i < slots.Count; i++)
+			{
+				string slot = slots[i];
+				string location = slot.Split(',')[0];
+
+				if (this._locations.ContainsKey(location))
+				{
+					valid_spot = true;
+				}
+				else
+				{
+					slots.RemoveAt(i--);
+				}
+			}
+
+			//if no valid spot was given, meeting cant be created
+			if (!valid_spot)
+			{
+				return false;
+			}
+
+
+			Meeting meeting = new Meeting(meeting_topic, owner_name, min_attendees, number_of_slots, number_of_invitees, slots, invitees);
+
+			_meetings.Add(meeting_topic, meeting);
+
+			string print = "Client " + owner_name + " created a meeting with topic: " + meeting_topic + ", with "
+				+ min_attendees + " required atendees, with slots: ";
 
 			foreach (string slot in slots)
 			{
@@ -162,10 +293,6 @@ namespace Server
 					meeting.MeetingRecords[slot].Add(client_name);
 					joined = true;
 				}
-				else
-				{
-					slots.Remove(slot);
-				}
 			}
 
 			if (joined)
@@ -180,9 +307,12 @@ namespace Server
 
 		public void AddRoom(string location, string name, int capacity)
 		{
-			_locations[location] = new Location(location);
+			if (!_locations.ContainsKey(location))
+			{
+				_locations[location] = new Location(location);
+			}
 
-			_locations[location]._rooms[name] = new Room(location, name, capacity);
+			_locations[location]._rooms.Add(name, new Room(location, name, capacity));
 		}
 
 	}
